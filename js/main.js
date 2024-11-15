85fcb59d0df9ed5d91f0026343a3fdd65610f822
HGThePublisherAlt <Marblemaz2011@gmail.com>
2024-11-15 15:49:24 -0800

    V1 First Publish.

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// Hello, you can take a look of what's is in here.
+// I'm new to JavaScript but I think I can do it.
+
+function fullscreenGame() {
+    document.getElementById("game").requestFullscreen();
+}
